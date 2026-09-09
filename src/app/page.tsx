a1792895d460e4fb87b16eb3bafa984f1f73c7c0
Hariom Chavale <chavalehariom@gmail.com>
2026-09-09 07:06:54 +0000

    https://i.imgur.com/9wYu3Sc.png this is the new one

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,7 +132,7 @@ export default function Home() {
           <div className="relative w-full max-w-6xl mx-auto h-[200px] sm:h-[400px] md:h-[500px] flex items-center justify-center">
             <div className="absolute inset-0 flex items-center justify-center">
               <img 
-                src="https://i.imgur.com/xGXlNAL.png" 
+                src="https://i.imgur.com/9wYu3Sc.png" 
                 alt="Incoming Frame" 
                 className="w-full h-full object-contain"
               />
